4fd36542c79478f338c6a38efe14f1ea5e87bb84
aiman1717a <aiman1717a@gmail.com>
2020-03-10 14:55:46 +0500

    Update field.js

--- dist/js/field.js
+++ dist/js/field.js
@@ -921,12 +921,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             var ref = this;
             var text = [];
             for (var i = 0; i < this.value.length; i++) {
+                console.log(this.value);
                 for (var letter in ref.letters) {
+                    console.log('letter', letter);
                     if (ref.letters.hasOwnProperty(letter)) {
                         if (this.value.charAt(i) === letter) {
                             text.push(ref.letters[letter]);
-                        } else {
-                            text.push(this.value.charAt(i));
                         }
                     }
                 }
