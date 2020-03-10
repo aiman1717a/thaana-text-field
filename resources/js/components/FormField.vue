8a278bec81b59b70d398cdf15ed0514ea081bd2b
aiman1717a <aiman1717a@gmail.com>
2020-03-10 13:44:28 +0500

    Update FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -20,6 +20,71 @@ export default {
     data(){
         return{
             my_field: null
+            letters: {
+                'q': 'ް',
+                'w': 'އ',
+                'e': 'ެ',
+                'r': 'ރ',
+                't': 'ތ',
+                'y': 'ޔ',
+                'u': 'ު',
+                'i': 'ި',
+                'o': 'ޮ',
+                'p': 'ޕ',
+                'a': 'ަ',
+                's': 'ސ',
+                'd': 'ދ',
+                'f': 'ފ',
+                'g': 'ގ',
+                'h': 'ހ',
+                'j': 'ޖ',
+                'k': 'ކ',
+                'l': 'ލ',
+                'z': 'ޒ',
+                'x': '×',
+                'c': 'ޗ',
+                'v': 'ވ',
+                'b': 'ބ',
+                'n': 'ނ',
+                'm': 'މ',
+                'Q': 'ޤ',
+                'W': 'ޢ',
+                'E': 'ޭ',
+                'R': 'ޜ',
+                'T': 'ޓ',
+                'Y': 'ޠ',
+                'U': 'ޫ',
+                'I': 'ީ',
+                'O': 'ޯ',
+                'P': '÷',
+                'A': 'ާ',
+                'S': 'ށ',
+                'D': 'ޑ',
+                'F': 'ﷲ',
+                'G': 'ޣ',
+                'H': 'ޙ',
+                'J': 'ޛ',
+                'K': 'ޚ',
+                'L': 'ޅ',
+                'Z': 'ޡ',
+                'X': 'ޘ',
+                'C': 'ޝ',
+                'V': 'ޥ',
+                'B': 'ޞ',
+                'N': 'ޏ',
+                'M': 'ޟ',
+                ',': '،',
+                ';': '؛',
+                '?': '؟',
+                '<': '>',
+                '>': '<',
+                '[': ']',
+                ']': '[',
+                '(': ')',
+                ')': '(',
+                '{': '}',
+                '}': '{',
+            }
         }
     },
     mixins: [FormField, HandlesValidationErrors],
@@ -38,6 +103,18 @@ export default {
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
+            var ref = this;
+            var text  = [];
+            for (var i = 0; i < this.value.length; i++) {
+                for(var letter in ref.letters){
+                    if (ref.letters.hasOwnProperty(letter)) {
+                        if(this.value.charAt(i) === letter){
+                            text.push(ref.letters[letter])
+                        }
+                    }
+                }
+            }
+            console.log('text', text);
             formData.append(this.field.attribute, this.value || '')
         },
 
