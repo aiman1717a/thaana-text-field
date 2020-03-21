e964da9b684ac4aab2fcc474566770e7f172ebea
aiman1717a <aiman1717a@gmail.com>
2020-03-21 20:33:26 +0500

    Update FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -2,150 +2,164 @@
     <default-field :field="field" :errors="errors">
         <template slot="field">
             <input
-                :id="field.name"
-                type="text"
-                class="w-full form-control form-input form-input-bordered thaana-keyboard"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value"
+                    :id="field.name"
+                    type="text"
+                    class="w-full form-control form-input form-input-bordered"
+                    :class="errorClasses"
+                    :placeholder="field.name"
+                    v-model="value"
+                    @keypress="value = $event.target.value"
+                    @keyup="value = $event.target.value"
+                    @keydown="value = $event.target.value"
+                    @input="value = $event.target.value"
             />
         </template>
     </default-field>
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-// import 'jtk.min.js'
-export default {
-    data(){
-        return{
-            letters: {
-                'q': 'ް',
-                'w': 'އ',
-                'e': 'ެ',
-                'r': 'ރ',
-                't': 'ތ',
-                'y': 'ޔ',
-                'u': 'ު',
-                'i': 'ި',
-                'o': 'ޮ',
-                'p': 'ޕ',
-                'a': 'ަ',
-                's': 'ސ',
-                'd': 'ދ',
-                'f': 'ފ',
-                'g': 'ގ',
-                'h': 'ހ',
-                'j': 'ޖ',
-                'k': 'ކ',
-                'l': 'ލ',
-                'z': 'ޒ',
-                'x': '×',
-                'c': 'ޗ',
-                'v': 'ވ',
-                'b': 'ބ',
-                'n': 'ނ',
-                'm': 'މ',
-                'Q': 'ޤ',
-                'W': 'ޢ',
-                'E': 'ޭ',
-                'R': 'ޜ',
-                'T': 'ޓ',
-                'Y': 'ޠ',
-                'U': 'ޫ',
-                'I': 'ީ',
-                'O': 'ޯ',
-                'P': '÷',
-                'A': 'ާ',
-                'S': 'ށ',
-                'D': 'ޑ',
-                'F': 'ﷲ',
-                'G': 'ޣ',
-                'H': 'ޙ',
-                'J': 'ޛ',
-                'K': 'ޚ',
-                'L': 'ޅ',
-                'Z': 'ޡ',
-                'X': 'ޘ',
-                'C': 'ޝ',
-                'V': 'ޥ',
-                'B': 'ޞ',
-                'N': 'ޏ',
-                'M': 'ޟ',
-                ',': '،',
-                ';': '؛',
-                '?': '؟',
-                '<': '>',
-                '>': '<',
-                '[': ']',
-                ']': '[',
-                '(': ')',
-                ')': '(',
-                '{': '}',
-                '}': '{',
+    import { FormField, HandlesValidationErrors } from 'laravel-nova'
+    export default {
+        data(){
+            return{
+                // letters: {
+                //     'q': 'ް',
+                //     'w': 'އ',
+                //     'e': 'ެ',
+                //     'r': 'ރ',
+                //     't': 'ތ',
+                //     'y': 'ޔ',
+                //     'u': 'ު',
+                //     'i': 'ި',
+                //     'o': 'ޮ',
+                //     'p': 'ޕ',
+                //     'a': 'ަ',
+                //     's': 'ސ',
+                //     'd': 'ދ',
+                //     'f': 'ފ',
+                //     'g': 'ގ',
+                //     'h': 'ހ',
+                //     'j': 'ޖ',
+                //     'k': 'ކ',
+                //     'l': 'ލ',
+                //     'z': 'ޒ',
+                //     'x': '×',
+                //     'c': 'ޗ',
+                //     'v': 'ވ',
+                //     'b': 'ބ',
+                //     'n': 'ނ',
+                //     'm': 'މ',
+                //     'Q': 'ޤ',
+                //     'W': 'ޢ',
+                //     'E': 'ޭ',
+                //     'R': 'ޜ',
+                //     'T': 'ޓ',
+                //     'Y': 'ޠ',
+                //     'U': 'ޫ',
+                //     'I': 'ީ',
+                //     'O': 'ޯ',
+                //     'P': '÷',
+                //     'A': 'ާ',
+                //     'S': 'ށ',
+                //     'D': 'ޑ',
+                //     'F': 'ﷲ',
+                //     'G': 'ޣ',
+                //     'H': 'ޙ',
+                //     'J': 'ޛ',
+                //     'K': 'ޚ',
+                //     'L': 'ޅ',
+                //     'Z': 'ޡ',
+                //     'X': 'ޘ',
+                //     'C': 'ޝ',
+                //     'V': 'ޥ',
+                //     'B': 'ޞ',
+                //     'N': 'ޏ',
+                //     'M': 'ޟ',
+                //     ',': '،',
+                //     ';': '؛',
+                //     '?': '؟',
+                //     '<': '>',
+                //     '>': '<',
+                //     '[': ']',
+                //     ']': '[',
+                //     '(': ')',
+                //     ')': '(',
+                //     '{': '}',
+                //     '}': '{',
+                // }
             }
-        }
-    },
-    mixins: [FormField, HandlesValidationErrors],
-
-    props: ['resourceName', 'resourceId', 'field'],
-
-    methods: {
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-            this.value = this.field.value || ''
         },
+        mixins: [FormField, HandlesValidationErrors],
 
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        fill(formData) {
-            // var ref = this;
-            // var my_value = this.value;
-            // var temp = [];
-            // for (var i = 0; i < my_value.length; i++) {
-            //     temp.push(ref.match(my_value.charAt(i)));
-            // }
-            // console.log('text', temp.join(''));
-            // formData.append(this.field.attribute, temp.join('') || '')
+        props: ['resourceName', 'resourceId', 'field'],
 
-            formData.append(this.field.attribute, this.value || '')
+        methods: {
+            /*
+             * Set the initial, internal value for the field.
+             */
+            setInitialValue() {
+                this.value = this.field.value || ''
+            },
+
+            /**
+             * Fill the given FormData object with the field's internal value.
+             */
+            fill(formData) {
+                // var ref = this;
+                // var my_value = this.value;
+                // var temp = [];
+                // for (var i = 0; i < my_value.length; i++) {
+                //     temp.push(ref.match(my_value.charAt(i)));
+                // }
+                // console.log('text', temp.join(''));
+                // formData.append(this.field.attribute, temp.join('') || '')
+
+                formData.append(this.field.attribute, this.value || '')
+            },
+
+            /**
+             * Update the field's internal value.
+             */
+            handleChange(value) {
+                this.value = value;
+            },
+            match: function (letter) {
+                var ref = this;
+                for (let [key, value] of Object.entries(ref.letters)) {
+                    if(letter === key){
+                        return value;
+                    }
+                }
+                return letter;
+            }
         },
-
-        /**
-         * Update the field's internal value.
-         */
-        handleChange(value) {
-            this.value = value;
-        },
-        match: function (letter) {
-            var ref = this;
-            for (let [key, value] of Object.entries(ref.letters)) {
-                if(letter === key){
-                    return value;
+        computed: {
+            keyboardDirection: function () {
+                console.log(this.field.status);
+                if(this.field.status === 'enable'){
+                    return {
+                        'direction': 'ltr',
+                    }
+                }
+                return {
+                    'direction': 'rtl',
                 }
             }
-            return letter;
+        },
+        // watch: {
+        //     value: function (val) {
+        //        console.log(val);
+        //     }
+        // },
+        mounted() {
+            thaanaKeyboard.defaultKeyboard = this.field.type;
+            thaanaKeyboard.setHandlerById(this.field.name, this.field.status);
+            if(this.field.status === 'enable'){
+                $('#' + this.field.name).css({'direction': 'rtl'})
+            }else{
+                $('#' + this.field.name).css({'direction': 'ltr'})
+            }
+            // $('.thaana-keyboard').thaana({keyboard: 'phonetic'});
         }
-    },
-    watch: {
-        value: function (val) {
-           console.log(val);
-        }
-    },
-    mounted() {
-        // console.log('Home mounted');
-        thaanaKeyboard.defaultKeyboard = this.field.type;
-        thaanaKeyboard.setHandlerByClass('thaana-keyboard', this.field.status);
-        // $('.thaana-keyboard').thaana({keyboard: 'phonetic'});
     }
-}
 </script>
-
-<style lang="scss" scoped>
-    .thaana-keyboard {
-        direction: rtl;
-    }
-</style>
