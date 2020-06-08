185911384c6de239ac34628d6bc4751cbe41f590
Mohamed Aiman <aiman1717a@gmail.com>
2020-06-08 15:12:07 -0700

    rtl bug fix

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -2,7 +2,7 @@
     <default-field :field="field" :errors="errors">
         <template slot="field">
             <input
-                    :id="field.name"
+                    :id="name"
                     type="text"
                     class="w-full form-control form-input form-input-bordered"
                     :class="errorClasses"
@@ -22,71 +22,7 @@
     export default {
         data(){
             return{
-                // letters: {
-                //     'q': 'ް',
-                //     'w': 'އ',
-                //     'e': 'ެ',
-                //     'r': 'ރ',
-                //     't': 'ތ',
-                //     'y': 'ޔ',
-                //     'u': 'ު',
-                //     'i': 'ި',
-                //     'o': 'ޮ',
-                //     'p': 'ޕ',
-                //     'a': 'ަ',
-                //     's': 'ސ',
-                //     'd': 'ދ',
-                //     'f': 'ފ',
-                //     'g': 'ގ',
-                //     'h': 'ހ',
-                //     'j': 'ޖ',
-                //     'k': 'ކ',
-                //     'l': 'ލ',
-                //     'z': 'ޒ',
-                //     'x': '×',
-                //     'c': 'ޗ',
-                //     'v': 'ވ',
-                //     'b': 'ބ',
-                //     'n': 'ނ',
-                //     'm': 'މ',
-                //     'Q': 'ޤ',
-                //     'W': 'ޢ',
-                //     'E': 'ޭ',
-                //     'R': 'ޜ',
-                //     'T': 'ޓ',
-                //     'Y': 'ޠ',
-                //     'U': 'ޫ',
-                //     'I': 'ީ',
-                //     'O': 'ޯ',
-                //     'P': '÷',
-                //     'A': 'ާ',
-                //     'S': 'ށ',
-                //     'D': 'ޑ',
-                //     'F': 'ﷲ',
-                //     'G': 'ޣ',
-                //     'H': 'ޙ',
-                //     'J': 'ޛ',
-                //     'K': 'ޚ',
-                //     'L': 'ޅ',
-                //     'Z': 'ޡ',
-                //     'X': 'ޘ',
-                //     'C': 'ޝ',
-                //     'V': 'ޥ',
-                //     'B': 'ޞ',
-                //     'N': 'ޏ',
-                //     'M': 'ޟ',
-                //     ',': '،',
-                //     ';': '؛',
-                //     '?': '؟',
-                //     '<': '>',
-                //     '>': '<',
-                //     '[': ']',
-                //     ']': '[',
-                //     '(': ')',
-                //     ')': '(',
-                //     '{': '}',
-                //     '}': '{',
-                // }
+
             }
         },
         mixins: [FormField, HandlesValidationErrors],
@@ -105,15 +41,6 @@
              * Fill the given FormData object with the field's internal value.
              */
             fill(formData) {
-                // var ref = this;
-                // var my_value = this.value;
-                // var temp = [];
-                // for (var i = 0; i < my_value.length; i++) {
-                //     temp.push(ref.match(my_value.charAt(i)));
-                // }
-                // console.log('text', temp.join(''));
-                // formData.append(this.field.attribute, temp.join('') || '')
-
                 formData.append(this.field.attribute, this.value || '')
             },
 
@@ -123,17 +50,16 @@
             handleChange(value) {
                 this.value = value;
             },
-            match: function (letter) {
-                var ref = this;
-                for (let [key, value] of Object.entries(ref.letters)) {
-                    if(letter === key){
-                        return value;
-                    }
-                }
-                return letter;
-            }
         },
         computed: {
+            name: function() {
+                return this.field.name.replace(/[^a-z0-9]/g, function(s) {
+                    var c = s.charCodeAt(0);
+                    if (c == 32) return '-';
+                    if (c >= 65 && c <= 90) return '_' + s.toLowerCase();
+                    return '__' + ('000' + c.toString(16)).slice(-4);
+                });
+            },
             keyboardDirection: function () {
                 console.log(this.field.status);
                 if(this.field.status === 'enable'){
@@ -146,20 +72,15 @@
                 }
             }
         },
-        // watch: {
-        //     value: function (val) {
-        //        console.log(val);
-        //     }
-        // },
+
         mounted() {
             thaanaKeyboard.defaultKeyboard = this.field.type;
-            thaanaKeyboard.setHandlerById(this.field.name, this.field.status);
+            thaanaKeyboard.setHandlerById(this.name, this.field.status);
             if(this.field.status === 'enable'){
-                $('#' + this.field.name).css({'direction': 'rtl'})
+                $('#' + this.name).css({'direction': 'rtl'})
             }else{
-                $('#' + this.field.name).css({'direction': 'ltr'})
+                $('#' + this.name).css({'direction': 'ltr'})
             }
-            // $('.thaana-keyboard').thaana({keyboard: 'phonetic'});
         }
     }
 </script>
